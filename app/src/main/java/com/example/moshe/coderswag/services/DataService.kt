d4c77212917f208c1ab7e1ba1d8ab824cd8fcf5f
Moshe Shimon <moshe31sh@gmail.com>
2017-10-31 15:43:38 +0200

    create data object

---
+++ app/src/main/java/com/example/moshe/coderswag/services/DataService.kt
@@ -0,0 +1,18 @@
+package com.example.moshe.coderswag.services
+
+import com.example.moshe.coderswag.model.Category
+
+/**
+ * Created by moshe on 31/10/2017.
+ */
+object DataService {
+
+    val categories = listOf(
+            Category("SHIRTS", "shirtimage"),
+            Category("HOODIES", "hoodiesimage"),
+            Category("HATS", "hatimage"),
+            Category("DIGITAL", "digitalgoodsimage")
+    )
+
+
+}
