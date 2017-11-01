3812f72b6e482c7b31d1af7bd9e0c8c2ae45bb6e
Moshe Shimon <moshe31sh@gmail.com>
2017-11-01 11:04:19 +0200

    add category list view

--- app/src/main/java/com/example/moshe/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/moshe/coderswag/controller/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.moshe.coderswag.controller
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.moshe.coderswag.R
+import com.example.moshe.coderswag.model.Category
+import com.example.moshe.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun getLayout(): Int {
         return R.layout.activity_main
+    }
+
+    override fun initView() {
+        adapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, DataService.categories)
+        CategoryListView.adapter = adapter
     }
 }
