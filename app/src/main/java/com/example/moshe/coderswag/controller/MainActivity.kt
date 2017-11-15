30753476ac7c4eb469e4b3b61d44241e75954003
Moshe Shimon <moshe31sh@gmail.com>
2017-11-15 11:13:01 +0200

    upgrade version

--- app/src/main/java/com/example/moshe/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/moshe/coderswag/controller/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.moshe.coderswag.controller
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import com.example.moshe.coderswag.R
 import com.example.moshe.coderswag.adapters.CategoryAdapter
+import com.example.moshe.coderswag.adapters.CategoryRecycleAdapter
 import com.example.moshe.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
-    lateinit var adapter: CategoryAdapter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun getLayout(): Int {
         return R.layout.activity_main
     }
 
     override fun initView() {
-        adapter = CategoryAdapter(this, DataService.categories)
-        CategoryListView.adapter = adapter
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
+        categoryListView.layoutManager = LinearLayoutManager(this)
+        categoryListView.setHasFixedSize(true)
     }
 }
